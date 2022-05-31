332943b2016814c004d4d85bcf1bd21b2ffa4378
XanMarta <gmxancrazy24114@gmail.com>
2022-05-31 15:07:59 +0700

    port forwarding for external hostname

---
+++ setup/rootless/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Use this script to start nginx container with rootless user
+
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 7080
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 7443
